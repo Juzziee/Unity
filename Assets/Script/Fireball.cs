bfa6f7445722328d2d6f3245c53412b679ffcfe6
Justin <Juzzwing@gmail.com>
2015-03-17 21:32:43 +1000

    More work on fireball script / animation

---
+++ Assets/Script/Fireball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	GameObject Player;
+	public GameObject explosionPrefab;
+	GameObject Explosion;
+	Vector3 impactPos;
+	float waitTime = 0.25f;
+
+
+	void Start () {
+		explosionPrefab = GameObject.Find ("CharacterContainer").GetComponent<Fireball> ().explosionPrefab;
+		Player = GameObject.Find ("Body");
+		Physics2D.IgnoreCollision (GetComponent<Collider2D>(), Player.GetComponent<Collider2D>());
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {	
+		impactPos = this.gameObject.transform.position;
+		Destroy (gameObject);
+		Explosion = (Instantiate (explosionPrefab, impactPos, transform.rotation)) as GameObject;
+		Explosion.name = "Explosion";
+		StartCoroutine (explodeFade(waitTime));
+	}
+
+	IEnumerator explodeFade(float waitTime) {
+		yield return new WaitForSeconds (waitTime);
+		Destroy (Explosion);
+
+	}
+}
